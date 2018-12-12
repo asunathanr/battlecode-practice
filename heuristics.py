ffa0394ecba0bcfd8c9ef6d38aabf21090887349
asunathanr <44170865+asunathanr@users.noreply.github.com>
2018-12-12 09:49:25 -0600

    Removed ref to point.py

--- heuristics.py
+++ heuristics.py
@@ -1,18 +1,21 @@
-from point import Point
-
 # File: heuristics.py
 # Author: Nathan Robertson
 # Purpose: Different heuristics for estimating best paths on a grid.
 
 
 def manhattan(current: (int, int), goal: (int, int)):
-    """ Use this heuristic when you can move in just four directions (up, down, left, right)  """
+    """
+    Use this heuristic when you can move in just four directions (up, down, left, right)
+    To move from current to goal without diagonals one must traverse x distance and y distance.
+    """
     return abs(current[0] - goal[0]) + abs(current[1] - goal[1])
 
 
 def diagonal(current: (int, int), goal: (int, int)):
-    """ Use this heuristic when you can move in eight directions:
-     (up, down, left, right, upper-left, upper-right, lower-left, lower-right) """
+    """
+    Use this heuristic when you can move in eight directions:
+     (up, down, left, right, upper-left, upper-right, lower-left, lower-right)
+    """
     return max(
         abs(current[0] - goal[0]),
         abs(current[1] - goal[1])
